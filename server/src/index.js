71df1ca004a652de0a1e15d98a978a2d5b713a09
Jennie <jentdev@gmail.com>
2024-01-02 19:58:04 -0800

    added server/index.js

---
+++ server/src/index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+
+const app = express();
+
+app.use(express.json()); // convert front-end data to json
+app.use(cors()); // set communication between front and backend
+
+app.listen();
