f85fdcc0e7b87c18d2f0ff0eccad6d40dc53a254
Jennie <jentdev@gmail.com>
2024-01-11 16:06:17 -0800

    updated server/index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,17 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
 app.use(express.json()); // convert front-end data to json
 app.use(cors()); // set communication between front and backend
+
+const mongoUri = process.env.MONGO_URI;
+
+mongoose.connect(mongoUri);
 
 app.listen(3001, () => console.log("server started!"));
