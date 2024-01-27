50b11eaccf7029de6774e6ddb59aae0e4ab300ee
Jennie <jentdev@gmail.com>
2024-01-26 18:06:48 -0800

    update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-    
+      <Router>
+        <Route path='/' element={} />
+      </Router>
     </div>
   );
 }
