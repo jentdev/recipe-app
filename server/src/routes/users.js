a94f9c1773cfe349d3c98860feb296fe6bf99a5c
Jennie Nguyen <jennienguyendev@gmail.com>
2024-01-26 18:06:48 -0800

    update App.js

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
+import dotenv from 'dotenv';
 
 import { UserModel } from '../models/User.js';
 
+dotenv.config();
+
 const router = express.Router(); // to set this up as a router
+
+const secret = process.env.SECRET;
+console.log(secret);
 
 router.post('/register', async(req, res) => {
     const { username, password } = req.body;
@@ -19,6 +25,8 @@ router.post('/register', async(req, res) => {
     // hash pw
     const hashedPw = await bcrypt.hash(password, 10);
 
+    console.log(hashedPw);
+
     // add user and hashed pw to database
     const newUser = new UserModel({ username, password: hashedPw });
     await newUser.save(); // make the changes to the db
@@ -29,6 +37,21 @@ router.post('/register', async(req, res) => {
 
 router.post('/login', async(req, res) => {
     const { username, password } = req.body;
+    const user = await UserModel.findOne({ username });
+
+    // if we don't find user
+    if (!user) {
+        return res.json({ message: "User doesn't exist."});
+    }
+
+    const isPwValid = await bcrypt.compare(password, user.password);
+
+    if(!isPwValid) {
+        return res.json({ message: "Username or Password is incorrect."});
+    }
+
+    const token = jwt.sign( {id: user._id }, secret);
+    res.json({ token, userID: user._id });
 });
 
 export { router as userRouter }; // export everything as a userRouter object
