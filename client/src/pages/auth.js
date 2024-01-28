a3b6aa8279a3df3f52c36f88981a01c276e93738
Jennie <jentdev@gmail.com>
2024-01-27 20:06:24 -0800

    updated auth page

--- client/src/pages/auth.js
+++ client/src/pages/auth.js
@@ -1,3 +1,19 @@
 export const Auth = () => {
-    return <div>Auth</div>
+    return <div className="auth">
+        <Login />
+        <Register />
+    </div>
+};
+
+// don't put in components folder because not shared between pages/only used in auth page
+const Login = () => {
+    return <div>Log in</div>;
+};
+
+const Register = () => {
+    return <div className="auth-container">
+        <form>
+            <h2>Register</h2>
+        </form>
+    </div>;
 };
